b911fd3461b508317a6ac7ab7c1e17bfd199a1c7
mmazz <mazzantimatiass@gmail.com>
2022-01-14 08:42:02 -0300

    [Fix] clickable status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "", 0, 0},
-    {"", "sb-net", 1, 16},
-    {"", "sb-battery", 5, 3},
+//    {"", "", 0, 0},
     {"", "sb-volume", 0, 10},
-    {"", "sb-keyboard", 0, 20},
-    {"", "sb-wifi", 5, 4},
-    {"", "sb-clock", 60, 1},
+//  {"", "sb-keyboard", 0, 20},
+    {"", "sb-internet",	5,	2},
+    {"", "sb-battery", 5, 3},
+    {"", "sb-weather",	18000,	4},
+    {"", "sb-clock", 60, 5},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
